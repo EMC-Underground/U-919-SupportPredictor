85c3453c08c3200a08e5f491a803cf87cde37100
Mark Rogov <mark@ntelligen.ly>
2016-09-18 16:02:41 -0400

    with cleanup

---
+++ bin/publish.prod.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+NAME=SupportPredictor
+cd src
+zip -Xrq ${NAME}.zip AlexaSkill.js index.js config.json node_modules/*
+#zip -Xrq ${NAME}.zip -x \*.zip *
+aws lambda update-function-code --function-name ${NAME} --zip-file fileb://${NAME}.zip
+rm ${NAME}.zip
+cd ..
